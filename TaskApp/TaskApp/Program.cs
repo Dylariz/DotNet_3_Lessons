fc96794de5a488edbd975ec02b090829b518d3d5
Dylariz <zarec-k@mail.ru>
2020-11-29 19:32:52 +0300

    Creating TaskApp

---
+++ TaskApp/TaskApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t1 = new Task<int>(() => {
+                int a = 0;
+                for (int i = 1; i <= 100; i++)
+                {
+                    a = a + i;
+                }
+                return a;
+            });
+            t1.Start();
+            Console.WriteLine(t1.Result);
+            Console.ReadLine();
+        }
+    }
+}
